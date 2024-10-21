0cfc002dddd288d0f691953a2e6c42621ffcfa8e
Sandeep Kumar Jakkaraju <sandeepkumar.jakkaraju@gmail.com>
2024-10-21 20:52:00 +0530

    Update AAI-520-Chatbot-Final-Project.py

--- AAI-520-Chatbot-Final-Project.py
+++ AAI-520-Chatbot-Final-Project.py
@@ -3,7 +3,7 @@
 
 # # Building a Chatbot
 
-# In this project, we will build a chatbot using conversations from Cornell University's [Movie Dialogue Corpus](https://www.cs.cornell.edu/~cristian/Cornell_Movie-Dialogs_Corpus.html). The main features of our model are LSTM cells, a bidirectional dynamic RNN, and decoders with attention. 
+# In this project, we will build a chatbot using conversations from Cornell University's [Movie Dialogue Corpus](https://www.kaggle.com/datasets/rajathmc/cornell-moviedialog-corpus). The main features of our model are LSTM cells, a bidirectional dynamic RNN, and decoders with attention. 
 # 
 # The conversations will be cleaned rather extensively to help the model to produce better responses. As part of the cleaning process, punctuation will be removed, rare words will be replaced with "UNK" (our "unknown" token), longer sentences will not be used, and all letters will be in the lowercase. 
 # 
@@ -22,7 +22,7 @@ tf.disable_v2_behavior()
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, InputLayer 
 
-# Most of the code to load the data is courtesy of https://github.com/suriyadeepan/practical_seq2seq/blob/master/datasets/cornell_corpus/data.py.
+# Most of the code to load the data is courtesy of https://www.kaggle.com/datasets/rajathmc/cornell-moviedialog-corpus.
 
 # ### Inspect and Load the Data
 
